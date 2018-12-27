c7def3e20c589de9b7486f31330130967dab7cc3
github_dongdongli <lidongdong64@gmail.com>
2018-12-27 11:54:34 +0800

    setup Express.js server

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'hello express.js'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
